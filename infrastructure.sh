a4b975661a88bf2f0853d368e4c4e7a9f83f1c3b
Mike Pham <mike.pham@proplogix.com>
2019-05-18 03:26:26 -0400

    updating scripts

--- infrastructure.sh
+++ infrastructure.sh
@@ -2,11 +2,17 @@
 
 ACTION="$1"
 
+GO=`which go`
 KUBECTL=`which kubectl`
 TERRAFORM=`which terraform`
 
+echo "go: ${GO}"
 echo "kubectl: ${KUBECTL}"
 echo "terraform: ${TERRAFORM}"
+
+if [ ! -f "${GO}" ]; then
+    echo "ERROR: go executable not found at '${GO}'"
+fi
 
 if [ ! -f "${KUBECTL}" ]; then
     echo "ERROR: kubectl executable not found at '${KUBECTL}'"
@@ -25,10 +31,10 @@ fi
 if [ "$ACTION" = "start" ]; then
     $TERRAFORM plan -out production
     $TERRAFORM apply production
-
+    
     echo 'export KUBECONFIG=$KUBECONFIG:~/.kube/config-production' >> ~/.zshrc
     source ~/.zshrc
-
+    
     $TERRAFORM output config-map > config-map-aws-auth.yaml
     $KUBECTL apply -f config-map-aws-auth.yaml
     $KUBECTL get nodes --watch
@@ -36,4 +42,5 @@ fi
 
 if [ "$ACTION" = "stop" ];  then
     $TERRAFORM destroy
+    rm config-map-aws-auth.yaml terraform.tfstate terraform.tfstate.backup
 fi
